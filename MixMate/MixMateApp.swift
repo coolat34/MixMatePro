6e86cffb756b57002e5ce1a93f3073ef429b0c8f
Chris Milne <milne_c1@sky.com>
2025-05-01 12:23:33 +0100

    Add files via upload

---
+++ MixMate/MixMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  MixMateApp.swift
+//  MixMate
+//
+//  Created by Chris Milne on 30/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct MixMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainMenu()
+        }
+    }
+}
